2113235b86dc4e198b78c83bad0d38ae440a06e3
omartannir123 <31383089-omartannir123@users.noreply.replit.com>
2024-01-30 16:10:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+   // loop through the array and print out the values
+    for (int i = 0; i < 5; i++)
+   
+      System.out.println("Welcome to Java");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
